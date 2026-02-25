a8f813fe6ac9e773a5aad11fc4f4b0f540e114fe
GhostReconxxx <ghostreconxxx10@gmail>
2026-02-24 22:01:44 -1100

    Fix the save button issue(doesn't check if there is any object in the idea array) when the user didn't click on it.

--- main.js
+++ main.js
@@ -1,14 +1,65 @@
 let addButon = document.getElementById("add-button")
 let registerButton = document.getElementById("register-button")
-let ideaTitle = document.getElementById("idea-title")
-let noteSection = document.getElementById("note-section")
 
-let noteSectionTitle = document.querySelector(".note-section-title > h3")
+
+
 let dateSection = document.querySelector(".note-section-footer > p")
+let container = document.getElementById("container")
+let leftContainer = document.querySelector(".left")
+let rightContainer = document.querySelector(".right")
+
+let leftRightContainer = document.querySelector(".left-right-container")
+
 
 let altiIdeas = []
 let ideas = []
 
+let generateIdea = () => {
+    return( leftContainer.innerHTML = 
+         ` 
+            <div class="title-section">
+                <h2>My Ideas</h2>
+                <div class="title-section-icone">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-search"></i>
+                    <i class="bi bi-emoji-laughing"></i>
+                </div>
+            </div>
+            <div class="idea"></div>
+
+         ` 
+    )
+} 
+
+let generateIdeasEditor = () =>{
+    return( rightContainer.innerHTML = 
+         ` 
+            <div class="title-section">
+                <h2>Ideas Editor</h2>
+                <div class="title-section-icone">
+                    <button type="button" id="add-button"><i class="bi bi-plus-lg" style="color: #2563eb;"></i></button>
+                    <button onclick = "saveWord()" id="register-button"><i class="bi bi-check-circle-fill"></i></button>
+                </div>
+            </div>
+            <div class="input-section">
+                <input id="idea-title" type="text" placeholder="Idea Title" autocomplete="off" aria-label="Idea Note" maxlength="100">
+                <div>
+                    <input  type="text" placeholder="Start typing your idea here...">
+                </div>
+            </div>
+        `
+        
+    )
+}
+
+generateIdea()
+generateIdeasEditor()
+
+let ideaTitle = document.getElementById("idea-title")
+let noteSectionTitle = document.querySelector(".note-section-title > h3")
+let noteSection = document.getElementById("note-section")
+
+console.log(ideaTitle)
 
 
 // ideaTitle.addEventListener("input", (event) => {
@@ -29,10 +80,17 @@ let ideas = []
         // content
         // date
 
+
+
+
+let containerIdea = document.querySelector(".idea")
+
 let genereateIdeaCard = ()  => {
     return(
-        noteSection.innerHTML = ideas.map((x) => {
-            let {title, content, date } = x
+        containerIdea.innerHTML = ideas.map((x) => {
+            let date = new Date()
+            let {title, content} = x
+            console.log(ideas.length)
             return(`   
                     <div>
                         <div class="note-section-title">
@@ -45,28 +103,25 @@ let genereateIdeaCard = ()  => {
                     </div>
                 `
             )
+            
         })
     )
     
     
 }
 
-registerButton.addEventListener("click", () => {
-    noteSection.classList.add("focus")
-    noteSection.classList.add("note-section")
+let saveWord = ()  =>{
+    // noteSection.classList.add("focus")
+    // noteSection.classList.add("note-section")
     let title = ideaTitle.value
-    let content = "I have to wok that."
-    let date = new Date()
+    console.log(title)
+    let content = "I have to work on that project"
     ideas.push({
         'title':title,
-        'content':content,
-        'date':date
+        'content':content
     })
+    console.log(ideas)
     genereateIdeaCard()
-    ideaTitle.innerText = " "
-    
 
-})
-// Now we need data to built our card
-    // We should get that data from when the focus event is active
-    // Add that data inside the idea array so we can use it to built the generate card function.
+    generateIdeasEditor()
+}
