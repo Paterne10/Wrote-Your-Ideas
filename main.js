bdfe0c42ee3f3060e94ef18bb24be4ecd0d4fe87
GhostReconxxx <ghostreconxxx10@gmail>
2026-02-26 21:51:47 -1100

    The user can write his idea title and see it  display on the left side container with a predefine content and the date.

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 let addButon = document.getElementById("add-button")
-let registerButton = document.getElementById("register-button")
-
-
-
 let dateSection = document.querySelector(".note-section-footer > p")
 let container = document.getElementById("container")
 let leftContainer = document.querySelector(".left")
 let rightContainer = document.querySelector(".right")
-
 let leftRightContainer = document.querySelector(".left-right-container")
-
-
-let altiIdeas = []
-let ideas = []
+let ideas = JSON.parse(localStorage.getItem("data")) || []
 
 let generateIdea = () => {
+
     return( leftContainer.innerHTML = 
-         ` 
+        ` 
             <div class="title-section">
                 <h2>My Ideas</h2>
                 <div class="title-section-icone">
@@ -27,8 +20,9 @@ let generateIdea = () => {
             </div>
             <div class="idea"></div>
 
-         ` 
+        ` 
     )
+    
 } 
 
 let generateIdeasEditor = () =>{
@@ -38,11 +32,11 @@ let generateIdeasEditor = () =>{
                 <h2>Ideas Editor</h2>
                 <div class="title-section-icone">
                     <button type="button" id="add-button"><i class="bi bi-plus-lg" style="color: #2563eb;"></i></button>
-                    <button onclick = "saveWord()" id="register-button"><i class="bi bi-check-circle-fill"></i></button>
+                    <button id="register-button"><i class="bi bi-check-circle-fill"></i></button>
                 </div>
             </div>
             <div class="input-section">
-                <input id="idea-title" type="text" placeholder="Idea Title" autocomplete="off" aria-label="Idea Note" maxlength="100">
+                <input id="idea-title" type="text" placeholder="Idea Title" autocomplete="off" maxlength="100">
                 <div>
                     <input  type="text" placeholder="Start typing your idea here...">
                 </div>
@@ -57,32 +51,6 @@ generateIdeasEditor()
 
 let ideaTitle = document.getElementById("idea-title")
 let noteSectionTitle = document.querySelector(".note-section-title > h3")
-let noteSection = document.getElementById("note-section")
-
-console.log(ideaTitle)
-
-
-// ideaTitle.addEventListener("input", (event) => {
-//     noteSection.classList.add("note-section")
-//     noteSection.classList.add("focus")
-//     noteSection.focus()
-//     let title = event.target.value
-//     let date = new Date()
-//     noteSectionTitle.textContent = title
-//     dateSection.textContent = date
-
-// })
-
-
-// each time that we select the input title we create a card idea
-    // That card ida is compose by :
-        // title   
-        // content
-        // date
-
-
-
-
 let containerIdea = document.querySelector(".idea")
 
 let genereateIdeaCard = ()  => {
@@ -92,7 +60,7 @@ let genereateIdeaCard = ()  => {
             let {title, content} = x
             console.log(ideas.length)
             return(`   
-                    <div>
+                    <div class = "note-section">
                         <div class="note-section-title">
                             <h3>${title}</h3>
                         </div>
@@ -105,23 +73,27 @@ let genereateIdeaCard = ()  => {
             )
             
         })
-    )
-    
-    
-}
+    )}
 
-let saveWord = ()  =>{
-    // noteSection.classList.add("focus")
-    // noteSection.classList.add("note-section")
+
+let registerButton = document.getElementById("register-button")
+let noteSection = document.querySelector("note-section")
+
+registerButton.addEventListener("click", () =>{
     let title = ideaTitle.value
-    console.log(title)
-    let content = "I have to work on that project"
-    ideas.push({
-        'title':title,
-        'content':content
-    })
-    console.log(ideas)
-    genereateIdeaCard()
+    let content = "I have to work on that project."
+    if(title !== ""){
+        ideas.push({
+            "title":title,
+            "content": content
+        });
+        genereateIdeaCard()
+        noteSection.classList.add("focus")
+        noteSection.classList.add("note-section")
+        
+    }else{
+        console.log("valeur vide")
+       
+    }
+} )
 
-    generateIdeasEditor()
-}
