93af3c2d7dd010c2d6a0f83398cd8262175edcbd
GhostReconxxx <ghostreconxxx10@gmail>
2026-03-28 14:56:21 -1100

    After being created the card idea can now be edit.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let leftContainer = document.querySelector(".left")
 let rightContainer = document.querySelector(".right")
 let leftRightContainer = document.querySelector(".left-right-container")
 let ideas = JSON.parse(localStorage.getItem("data")) || []
+let currentId = null
 
 let generateIdea = () => {
 
@@ -25,13 +26,20 @@ let generateIdea = () => {
     )
 } 
 
+let createNewIdea = ()  => {
+    ideaTitle.value = ""
+    ideaContent.value = ""
+    currentId = null
+    ideaTitle.focus()
+}
+
 let generateIdeasEditor = () =>{
     return( rightContainer.innerHTML = 
          ` 
             <div class="title-section">
                 <h2>Ideas Editor</h2>
                 <div class="title-section-icone">
-                    <button type="button" id="add-button"><i class="bi bi-plus-lg" style="color: #2563eb;"></i></button>
+                    <button type="button" id="add-button" onclick = "createNewIdea()" ><i class="bi bi-plus-lg" style="color: #2563eb;"></i></button>
                     <button id="register-button"><i class="bi bi-check-circle-fill"></i></button>
                 </div>
             </div>
@@ -39,6 +47,7 @@ let generateIdeasEditor = () =>{
                 <input id="idea-title" type="text" placeholder="Idea Title" autocomplete="off" maxlength="100">
                 <div>
                     <input id="idea-content"  type="text" placeholder="Start typing your idea here...">
+                    
                 </div>
             </div>
         `
@@ -51,20 +60,20 @@ generateIdeasEditor()
 
 
 let ideaTitle = document.getElementById("idea-title")
-let noteSectionTitle = document.querySelector(".note-section-title > h3")
 let containerIdea = document.querySelector(".idea")
 let ideaContent = document.getElementById("idea-content")
+
 
 let genereateIdeaCard = ()  => {
     return(
         containerIdea.innerHTML = ideas.map((x) => {
             let date = new Date()
-            let {title, content} = x
+            let {title, content, id} = x
             console.log(ideas.length)
             return(`   
-                    <div class = "note-section">
+                    <div class = "note-section" onclick="selectCard(${id})" >
                         <div class="note-section-title">
-                            <h3>${title}</h3>
+                            <h3 onclick="selectCard(${id})"  id="${id}" >${title}</h3>
                         </div>
                         <p class="note-section-content">${content}</p>
                         <div class="note-section-footer">
@@ -79,28 +88,51 @@ let genereateIdeaCard = ()  => {
 
 genereateIdeaCard()
 
-console.log(ideas)
+let selectCard = (id) => {
+    let selectItem = id
+    let search = ideas.find((x) => x.id === selectItem)
+    ideaTitle.value = search.title
+    ideaContent.value = search.content
+    currentId = id
+}
+
+
 
 let registerButton = document.getElementById("register-button")
-let noteSection = document.querySelector("note-section")
+let noteSectionTitle = document.querySelector(".note-section-title > h3")
+let noteSectionContent = document.querySelector(".note-section-content")
+let noteSectionDate = document.querySelector(".note-section-footer > p")
+let id = 0
 
 registerButton.addEventListener("click", () =>{
     let title = ideaTitle.value
     let content = ideaContent.value
-
-    if(title !== ""){
+    if (title === "") return
+    if(currentId === null){
         ideas.push({
+            "id": id += 1,  
             "title":title,
-            "content": content
+            "content": content  
         });
-        genereateIdeaCard()
-        localStorage.setItem("data", JSON.stringify(ideas))
-        // noteSection.classList.add("focus")
-        // noteSection.classList.add("note-section")
-        
-    }else{
-        console.log("valeur vide")
-       
+        console.log("I just add a new card")
+        alert("new idea created")
+    }else {
+        let search = ideas.find((x) => x.id === currentId)
+        search.title = ideaTitle.value
+        search.content = ideaContent.value
+        currentId = null
+        alert("idea updated")
     }
-} )
+
+    genereateIdeaCard() 
+    ideaTitle.value = ""
+    ideaContent.value = ""
+    localStorage.setItem("data", JSON.stringify(ideas))
+});
+
+
+
+
+
+
 
